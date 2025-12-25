85fb2f735f6781f2a13542003fbf9288df7fcb07
Thomas Burguiere <thomas.burguiere1@swisscom.com>
2025-12-25 08:48:03 +0000

    chore: remove non kt code, reenable renovate

--- libs/carbonlog-frontend-lib/src/lib/store/actions.ts
+++
@@ -1,9 +0,0 @@
-export class FetchMeasurements {
-    static readonly type = "[Measurements] Fetch";
-}
-
-export class SaveMeasurement {
-    static readonly type = "[Measurements] Save";
-    constructor(public co2Kg: number) {
-    }
-}
