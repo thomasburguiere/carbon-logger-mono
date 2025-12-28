6942e562622572c753671bb553825b9f378b101c
Thomas Burguiere <thomas.burguiere1@swisscom.com>
2025-12-28 16:26:02 +0100

    fix: jackson serialization config

--- apps/carbonlog-backend/src/main/kotlin/ch/burguiere/carbonlog/carbonlogbackend/webflux/JacksonConfig.kt
+++ apps/carbonlog-backend/src/main/kotlin/ch/burguiere/carbonlog/carbonlogbackend/webflux/JacksonConfig.kt
@@ -1,19 +1,16 @@
 package ch.burguiere.carbonlog.carbonlogbackend.webflux
 
-import com.fasterxml.jackson.annotation.JsonInclude
-//import com.fasterxml.jackson.databind.ObjectMapper
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-//import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.KotlinModule
 
 @Configuration
 open class JacksonConfig {
     @Bean
     open fun customJsonMapper(): JsonMapper {
         return JsonMapper.builder()
+            .addModule(KotlinModule.Builder().build())
             .build()
     }
 }
