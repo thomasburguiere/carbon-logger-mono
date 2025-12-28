22d5d028e457c507c0fa7270fc2d9527f5149226
Thomas Burguiere <thomas.burguiere@gmail.com>
2025-12-28 12:51:42 +0000

    refactor: use spring boot4, use gradle init's monorepo buildSrc approach (#38)

--- libs/carbonlog-backend/repository/build.gradle.kts
+++ libs/carbonlog-backend/repository/build.gradle.kts
@@ -1,49 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val javaVersion: String by project
-
 plugins {
-    id("org.springframework.boot") apply false
-    id("io.spring.dependency-management")
-    id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.kotlin.plugin.spring")
+    id("buildlogic.kotlin-library-conventions")
 }
 
-group = "ch.burguiere.carbonlog"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
-
-repositories {
-    mavenCentral()
-}
-
-dependencyManagement {
-    imports {
-        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-    }
-}
 
 dependencies {
-
     implementation("org.mongodb:mongodb-driver-reactivestreams")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("io.projectreactor:reactor-core")
-    implementation("org.springframework:spring-context")
-
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
 
     api(project(":libs:carbonlog-base"))
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = javaVersion
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
--- libs/carbonlog-base/build.gradle.kts
+++ libs/carbonlog-base/build.gradle.kts
@@ -1,43 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val javaVersion: String by project
-
 plugins {
-    id("org.springframework.boot") apply false
-    id("io.spring.dependency-management")
-    id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.kotlin.plugin.spring")
-}
-
-group = "ch.burguiere.carbonlog"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
-
-repositories {
-    mavenCentral()
-}
-
-dependencyManagement {
-    imports {
-        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-    }
+    id("buildlogic.kotlin-library-conventions")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
     api(project(":libs:carbonlog-converter"))
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = javaVersion
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
