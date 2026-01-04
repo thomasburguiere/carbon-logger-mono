1b792fb64b11a9ec60bf54feb92b46c72392da14
Thomas Burguiere <thomas.burguiere@gmail.com>
2026-01-04 13:12:19 +0100

    fix: allow explicit null setting of description when updating measurement (#65)

--- apps/carbonlog-backend/src/main/kotlin/ch/burguiere/carbonlog/carbonlogbackend/repository/MongoCarbonMeasurementsRepository.kt
+++ apps/carbonlog-backend/src/main/kotlin/ch/burguiere/carbonlog/carbonlogbackend/repository/MongoCarbonMeasurementsRepository.kt
@@ -1,19 +1,21 @@
 package ch.burguiere.carbonlog.carbonlogbackend.repository
 
 import ch.burguiere.carbonlog.model.CarbonMeasurement
-import ch.burguiere.carbonlog.carbonlogbackend.repository.MongoCarbonMeasurementsRepository.Fields
 import ch.burguiere.carbonlog.carbonlogbackend.repository.MongoCarbonMeasurementsRepository.Fields.CO2_KG
 import ch.burguiere.carbonlog.carbonlogbackend.repository.MongoCarbonMeasurementsRepository.Fields.ID
 import ch.burguiere.carbonlog.carbonlogbackend.repository.MongoCarbonMeasurementsRepository.Fields.INPUT_DESCRIPTION
 import ch.burguiere.carbonlog.carbonlogbackend.repository.MongoCarbonMeasurementsRepository.Fields.TIMESTAMP
 import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.Updates
 import com.mongodb.client.model.Updates.combine
 import com.mongodb.client.model.Updates.set
 import com.mongodb.reactivestreams.client.MongoCollection
 import org.bson.BsonDateTime
 import org.bson.BsonDocument
 import org.bson.BsonDouble
+import org.bson.BsonNull
 import org.bson.BsonString
+import org.bson.conversions.Bson
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toFlux
@@ -53,17 +55,18 @@ class MongoCarbonMeasurementsRepository(private val collection: MongoCollection<
         .toMono()
         .then()
 
-    override fun updateMeasurement(id: String, measurement: CarbonMeasurement): Mono<Void> {
-        return collection.findOneAndUpdate(
-            eq(ID, BsonString(id)),
-            combine(
-                set(CO2_KG, BsonDouble(measurement.co2Kg)),
-                set(INPUT_DESCRIPTION, BsonString(measurement.inputDescription)),
-                set(TIMESTAMP, BsonDateTime(measurement.dt.toEpochMilli()))
-            )
+    override fun updateMeasurement(id: String, measurement: CarbonMeasurement): Mono<Void> = collection.findOneAndUpdate(
+        eq(ID, BsonString(id)),
+        combine(
+            set(CO2_KG, BsonDouble(measurement.co2Kg)),
+            set(TIMESTAMP, BsonDateTime(measurement.dt.toEpochMilli())),
+            when (measurement.inputDescription != null) {
+                true -> set(INPUT_DESCRIPTION, BsonString(measurement.inputDescription))
+                else -> set(INPUT_DESCRIPTION, BsonNull())
+            },
         )
-            .toMono().then()
-    }
+    )
+        .toMono().then()
 }
 
 private fun BsonDocument.parseMeasurement(): CarbonMeasurement =
@@ -72,7 +75,7 @@ private fun BsonDocument.parseMeasurement(): CarbonMeasurement =
         co2Kg = this.getDouble(CO2_KG).value,
         dt = Instant.ofEpochMilli(this.getDateTime(TIMESTAMP).value),
         inputDescription = when {
-            this.containsKey(INPUT_DESCRIPTION) -> this.getString(INPUT_DESCRIPTION).value
+            this.containsKey(INPUT_DESCRIPTION) && !this.isNull(INPUT_DESCRIPTION) -> this.getString(INPUT_DESCRIPTION).value
             else -> null
         }
     )
